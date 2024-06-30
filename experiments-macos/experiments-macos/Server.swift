0301b1280d069715382f86a3faea839125512e1a
Alexander Johansson <alexander@shape.dk>
2024-06-30 18:27:28 +0200

    Calibration status threshold changed

--- experiments-macos/experiments-macos/Server.swift
+++ experiments-macos/experiments-macos/Server.swift
@@ -51,7 +51,7 @@ func routes(_ app: Application) throws {
         print("Received data: \(data)")
         saveDataToFile(data)
         
-        let sufficientCalibration = data.calibration_status >= 2
+        let sufficientCalibration = data.calibration_status >= 1
         DataViewModel.sharedSingleton.markDataReceived(data: data, sufficientCalibration: sufficientCalibration)
         
         return .ok
