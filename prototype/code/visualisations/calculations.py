12c114db2e1468967e3753e27495b4a6d78ed7ea
Alexander Johansson <alexander@shape.dk>
2024-07-03 14:19:12 +0200

    Detached quat calculations and scaling

---
+++ prototype/code/visualisations/calculations.py
@@ -0,0 +1,40 @@
+import math
+
+
+# Quaternion to euler angles
+def normalize(x, y, z, w):
+    magnitude = math.sqrt(x * x + y * y + z * z + w * w)
+    return (x / magnitude, y / magnitude, z / magnitude, w / magnitude)
+
+def quaternion_to_euler(x, y, z, w):
+    x, y, z, w = normalize(x, y, z, w)
+
+    sqw = w * w
+    sqx = x * x
+    sqy = y * y
+    sqz = z * z
+
+    euler_x = math.atan2(2.0 * (x * y + z * w), (sqx - sqy - sqz + sqw))
+    euler_y = math.asin(-2.0 * (x * z - y * w) / (sqx + sqy + sqz + sqw))
+    euler_z = math.atan2(2.0 * (y * z + x * w), (-sqx - sqy + sqz + sqw))
+
+    return (math.degrees(euler_x), math.degrees(euler_y), math.degrees(euler_z))
+
+
+# Scaling of input value
+def scale_value(value, scale, min_value, max_value, clamp=False):
+    absolute = abs(value)
+    if clamp:
+        clamped = max(min_value, min(absolute, max_value))
+    else:
+        clamped = absolute
+    return round((clamped / max_value) * scale)
+
+
+def scale_roll(roll, scale):
+    return scale_value(roll, scale, 0.0, 90.0)
+
+
+def scale_pitch(pitch, scale):
+    return scale_value(pitch, scale, 0.0, 90.0)
+
